235fda5e9a4472762129c928b7acc38fb8f4e012
Tim Donohue <tdonohue@duraspace.org>
2016-08-25 16:06:31 -0500

    Default to latest PostgreSQL available on Ubuntu 16.04

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,7 +48,7 @@ dspace::owner { 'dspace':
 # Create a PostgreSQL database named 'dspace'
 #--------------------------------------------
 dspace::postgresql_db { 'dspace':
-  version           => '9.4',
+  version           => '9.5',
   postgres_password => 'postgres', # DB root password (for postgres superuser)
   user              => 'dspace',   # DB owner
   password          => 'dspace',   # DB owner password
